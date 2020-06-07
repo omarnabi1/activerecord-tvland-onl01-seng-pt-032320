103026795839649e60987bd29599ef0f6e4ac6cd
Omar Nabi <onabi20@gmail.com>
2020-06-07 20:25:19 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,6 +6,12 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
   
+  def list_roles
+   characters.collect do |character|
+       "#{character.name} - #{character.show.name}"
+   end
+  end
+  
   
   
 end
