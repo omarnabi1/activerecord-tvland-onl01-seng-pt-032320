4a895fcb08abb7158a12afc7ba6775ac46e49c04
Omar Nabi <onabi20@gmail.com>
2020-06-07 20:41:39 +0000

    Done.

---
+++ db/migrate/004_create_characters.rb
@@ -0,0 +1,11 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+    
+    def change
+      create_table :characters do |character| 
+        character.string :name
+        character.integer :actor_id
+        character.integer :show_id
+      end
+    end
+  end
+  
